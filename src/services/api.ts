a67062ee1a2f92080671985acf20c50a909659be
kfree23 <krystalfreeman50@gmail.com>
2026-08-03 11:09:35 -0500

    fetch ESPN base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
-// War Room — API service layer
-// TODO(krystal): you write this
+export const BASE_URL = 'https://site.api.espn.com/apis/v2/sports/basketball/nba'
 
-export const BASE_URL = 'https://site.api.espn.com/apis/v2/sports/basketball/nba/standings'
 
-// TODO(krystal): you write this
-// export async function fetchStandings(): Promise<Standing[]> { }
+export async function fetchStandings() {
+    const response = await fetch(`${BASE_URL}/standings`);
+    if(!response.ok) {
+        throw new Error(`Something went wrong: ${response.status}`)
+    }
+    const data = await response.json();
+    return {
+        east: data.children[0].standings.entries,
+        west: data.children[1].standings.entries
+    }
+}
