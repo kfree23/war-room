ff64b5072798a772d69f7f4b61fd38543e3ece93
kfree23 <krystalfreeman50@gmail.com>
2026-09-09 23:50:30 -0500

    error test

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,23 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://site.api.espn.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/espn-site': {
-        target: 'https://site.api.espn.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/espn-site/, '')
-      },
-      '/espn-athlete': {
-        target: 'https://site.web.api.espn.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/espn-athlete/, '')
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://site.api.espn.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     },
+  //     '/espn-site': {
+  //       target: 'https://site.api.espn.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/espn-site/, '')
+  //     },
+  //     '/espn-athlete': {
+  //       target: 'https://site.web.api.espn.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/espn-athlete/, '')
+  //     }
+  //   }
+  // }
 })
